bf337720481c833ca157fb7cfcf08f9fab7e9fb9
ManikantaPrince <102205618+ManikantaPrince@users.noreply.github.com>
2022-03-24 16:44:19 +0530

    Update UserDao.java

--- src/main/java/com/user/UserDao.java
+++ src/main/java/com/user/UserDao.java
@@ -2,7 +2,7 @@ package com.user;
 
 public class UserDao {
     public int save(){
-        System.out.println("write logic to save object in db but this is stub method");
+        System.out.println("write logic to save object in db but this is stub meeeethod");
         return 1;
     }
 }
